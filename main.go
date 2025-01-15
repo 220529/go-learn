9bcbd626a2a29d057b4e467accb9ee6c4c755a6c
CHINAMI-JH74J9H\EDY <lkx>
2025-01-15 11:15:59 +0800

    test: local go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!!",
+		})
+	})
+
+	// 启动服务
+	r.Run(":8080")
+}
